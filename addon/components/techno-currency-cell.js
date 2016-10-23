02ae6c6068f1f4727db882927f18286d7deb603b
Michael Latta (TechnoMage) <mlatta@technomage.com>
2016-10-23 16:30:41 -0600

    Switch to ember add on for accounting.js

--- addon/components/techno-currency-cell.js
+++ addon/components/techno-currency-cell.js
@@ -5,12 +5,14 @@ import accounting from 'accounting';
 export default Ember.Component.extend({
   layout: layout,
   tagName: 'td',
-  formattedValue: Ember.computed({
+  formattedValue: Ember.computed('value', {
     get: function() {
       let v = this.get('value');
+      // console.log('@@@@ Formatting currency cell: ',v);
       return accounting.formatMoney(v);
     },
     set: function(key,value) {
+      // console.log('@@@@ Setting currency cell: ',value);
       return accounting.unformat(value);
     }
   })
